6d2d69012d40e3d9755a57d4d2b161bff96be6de
steve-naherny <64172067+steve-naherny@users.noreply.github.com>
2022-02-24 22:04:38 -0600

    Added UnitConverter Class

---
+++ app/src/main/java/com/example/myapplication/UnitConverter.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+public class UnitConverter() {
+    private val units = mutableMapOf<String, Unit>()
+    init {
+        addUnit("Base", Category.Volume, 1.0)
+        addUnit("Teaspoon", Category.Volume, 1.0)
+        addUnit("Tablespoon", Category.Volume, 3.0)
+        addUnit("Cup", Category.Volume, 48.6922)
+        addUnit("Ounce", Category.Volume, 6.0)
+        addUnit("Apple", Category.PseudoVolume, 36.0)
+    }
+
+    fun convert(value: Double, from: String, to: String): Double {
+        val unitFrom = units.getValue(from)
+        val unitTo = units.getValue(to)
+        if (unitTo != null && unitFrom != null) {
+            println("Invalid Unit used. $from:$to")
+            return 0.0
+        }
+
+        val baseValue = value * unitFrom.conversionFromBase
+        val conversionToBase = 1.0 / unitTo.conversionFromBase
+        return unitTo.conversionFromBase * baseValue
+    }
+
+    private fun addUnit(name: String, category: Category, conversionFromBase: Double)
+    {
+        units[name] = Unit(name, category, conversionFromBase)
+    }
+
+    enum class Category {
+        Volume,
+        PseudoVolume
+    };
+
+    public class Unit(
+        val name: String,
+        val type: Category,
+        var conversionFromBase: Double
+    ){}
+}
